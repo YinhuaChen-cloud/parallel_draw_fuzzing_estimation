f85fd5f8ba1d1627ad016db3717e3b6064fd94e9
YinhuaChen-cloud <11612127@mail.sustech.edu.cn>
2025-01-11 21:19:51 +0800

    update

--- parallel_draw_seed.py
+++ parallel_draw_seed.py
@@ -76,80 +76,8 @@ for i in range(len(PROGRAMS_list)):
 
 PROGRAMS = PROGRAMS_list[0]
 
-############################################### 2. 并行读取绘图所需数据 (plot_data) ############################### 
+############################################### 2. 对所有文件 ############################### 
 
-# 一个全局变量，被所有并行任务共享，标识已经完成的任务数量
-finished_tasks = multiprocessing.Value('i', 0)  # 'i' 表示整数
-
-# 被并行执行的函数 (收集 queue， crash 的数据，包括 time/execs 维度) --------------------------------------------------------------- start 
-def collect_data_worker(FUZZER, TARGET, PROGRAM, TIME, parallel_id):
-    # 当前这个 PROGRAM-FUZZER-TIME 所对应的 queue 文件夹路径
-    queue_path = FUZZER + "/" + TARGET + "/" + PROGRAM + "/" + TIME + "/findings/" + parallel_id + "/queue"
-    # 当前这个 PROGRAM-FUZZER-TIME 所对应的 crash 文件夹路径
-    crash_path = FUZZER + "/" + TARGET + "/" + PROGRAM + "/" + TIME + "/findings/" + parallel_id + "/crash"
-
-
-
-
-
-
-    # 打印信息，表示这个数据收集任务已完成
-    with finished_tasks.get_lock():
-        finished_tasks.value += 1
-        print(f"{finished_tasks.value} finish {FUZZER}-{TARGET}-{PROGRAM}-{TIME}-{parallel_id} data collect")
-        sys.stdout.flush()
-    # 返回存储数据的 DataFrame，也就是 df，前面的几个元素是为了标识这个 df 属于哪个 PROGRAM-FUZZER-TIME
-    return (FUZZER, TARGET, PROGRAM, TIME, parallel_id, df)
-# 被并行执行的函数 (收集 queue， crash 的数据，包括 time/execs 维度) --------------------------------------------------------------- end
-
-# 获取当前机器上的 CPU cores 总数，方便后续并行操作
-num_cores = multiprocessing.cpu_count()
-print(f'CPU 核心数量: {num_cores}')
-sys.stdout.flush()
-
-# 创建一个进程池，池中进程的数量等于 CPU 核心数量
-pool = multiprocessing.Pool(num_cores)
-
-# 储存收集数据结果的队列
-results = []
-
-# 任务数计数器，也可以叫任务序号计数器
-task_count = 0
-
-# 为每一个 program-fuzzer-repeat_time 收集 plot_data 数据
-for PROGRAM in PROGRAMS:
-    for FUZZER in FUZZERS:
-        # 收集这个 PROGRAM-FUZZER 的所有 REPEAT_times 的 plot_data 数据
-
-        # 找到包含当前 PROGRAM 的 TARGETS
-        for TARGET in TARGETS:
-            path = FUZZER + "/" + TARGET
-            thePROGRAMS = getsubdir(path)
-            for thePROGRAM in thePROGRAMS:
-                if thePROGRAM != PROGRAM:
-                    continue
-
-                # 验证 fuzzing result 的 repeat_times 是否和我们的 0.配置部分 一致
-                path = FUZZER + "/" + TARGET + "/" + PROGRAM
-                TIMES = getsubdir(path)
-                assert(len(TIMES) == REPEAT)
-                for TIME in TIMES:
-                    assert(int(TIME) < REPEAT)
-
-                # 分配一个 CPU cores，让它收集当前 PROGRAM-FUZZER-TIME 的 plot_data 信息，结果存放于 results 列表
-                for TIME in TIMES:
-                    for parallel_id in PARALLEL_IDS:
-                        result = pool.apply_async(collect_data_worker, (FUZZER, TARGET, PROGRAM, TIME, parallel_id))
-                        task_count += 1
-                        results.append(result)
-
-# 打印看看一共有多少个并行任务在运行
-print(f"================== There are {len(results)} data collect tasks in total ==================")
-sys.stdout.flush()
-
-# 等待所有并行任务结束
-for result in results:
-    result.wait()
 
 sys.exit(0)
 
